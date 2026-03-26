9e9a503f9806377e6605997621439df06008adcd
localboy <phoenixdark318@gmail.com>
2026-03-26 10:07:46 +0530

    Initial commit - QR Code Generator Flask App

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ import qrcode
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
+    qr_path = None
+
     if request.method == 'POST':
-        data = request.form['data']
-        img = qrcode.make(data)
+        text = request.form['data']   # get input from form
+
+        # generate QR code
+        img = qrcode.make(text)
+
+        # save image
         img.save("static/qr.png")
-        return render_template('index.html', qr="static/qr.png")
-    
-    return render_template('index.html')
+
+        qr_path = "static/qr.png"
+
+    return render_template('index.html', qr=qr_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
